9e5475debc7d262426839c775f644f34c6a0d353
mpstaton <mpstaton@gmail.com>
2017-01-08 18:40:48 -0800

    Got the BlogPosts code working

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
-const {ShoppingList, Recipes} = require('./models');
+const {BlogPosts} = require('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -12,38 +12,31 @@ const app = express();
 // log the http layer
 app.use(morgan('common'));
 
-// we're going to add some items to ShoppingList
+// we're going to add some items to BlogPosts
 // so there's some data to look at
-ShoppingList.create('beans', 2);
-ShoppingList.create('tomatoes', 3);
-ShoppingList.create('peppers', 4);
-
-// adding some recipes to `Recipes` so there's something
-// to retrieve.
-Recipes.create(
-  'boiled white rice', ['1 cup white rice', '2 cups water', 'pinch of salt']);
-Recipes.create(
-  'milkshake', ['2 tbsp cocoa', '2 cups vanilla ice cream', '1 cup milk']);
+BlogPosts.create('January Blog', 'Lorem Ipsum for January', 'Michael Staton');
+BlogPosts.create('February Blog', 'Lorem Ipsum for February', 'Michael Staton');
+BlogPosts.create('March Blog', 'Lorem Ipsum for March', 'Michael Staton');
 
 // when the root of this router is called with GET, return
-// all current ShoppingList items
-app.get('/shopping-list', (req, res) => {
-  res.json(ShoppingList.get());
+// all current BlogPosts items
+app.get('/blog-posts', (req, res) => {
+  res.json(BlogPosts.get());
 });
 
-app.post('/shopping-list', jsonParser, (req, res) => {
+app.post('/blog-posts', jsonParser, (req, res) => {
   // ensure `name` and `budget` are in request body
-  const requiredFields = ['name', 'budget'];
+  const requiredFields = ['title', 'content', 'author', 'publishDate'];
   for (let i=0; i<requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`
+      const message = `Missing \`${field}\` in BlogPost request body`
       console.error(message);
       return res.status(400).send(message);
     }
   }
 
-  const item = ShoppingList.create(req.body.name, req.body.budget);
+  const item = BlogPosts.create(req.body.name, req.body.budget);
   res.status(201).json(item);
 });
 
@@ -51,13 +44,13 @@ app.post('/shopping-list', jsonParser, (req, res) => {
 // required fields. also ensure that item id in url path, and
 // item id in updated item object match. if problems with any
 // of that, log error and send back status code 400. otherwise
-// call `ShoppingList.update` with updated item.
-app.put('/shopping-list/:id', jsonParser, (req, res) => {
-  const requiredFields = ['name', 'budget', 'id'];
+// call `BlogPosts.update` with updated item.
+app.put('/blog-posts/:id', jsonParser, (req, res) => {
+  const requiredFields = ['title', 'content', 'author', 'publishDate'];
   for (let i=0; i<requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`
+      const message = `Missing \`${field}\` in BlogPost request body`
       console.error(message);
       return res.status(400).send(message);
     }
@@ -69,72 +62,21 @@ app.put('/shopping-list/:id', jsonParser, (req, res) => {
     console.error(message);
     return res.status(400).send(message);
   }
-  console.log(`Updating shopping list item \`${req.params.id}\``);
-  const updatedItem = ShoppingList.update({
+  console.log(`Updating BlogPost \`${req.params.id}\``);
+  const updatedItem = BlogPosts.update({
     id: req.params.id,
-    name: req.body.name,
-    budget: req.body.budget
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author
   });
   res.status(204).json(updatedItem);
 });
 
 // when DELETE request comes in with an id in path,
-// try to delete that item from ShoppingList.
-app.delete('/shopping-list/:id', (req, res) => {
-  ShoppingList.delete(req.params.id);
+// try to delete that item from BlogPosts.
+app.delete('/blog-posts/:id', (req, res) => {
+  BlogPosts.delete(req.params.id);
   console.log(`Deleted shopping list item \`${req.params.ID}\``);
-  res.status(204).end();
-});
-
-
-app.get('/recipes', (req, res) => {
-  res.json(Recipes.get());
-});
-
-app.post('/recipes', jsonParser, (req, res) => {
-  // ensure `name` and `budget` are in request body
-  const requiredFields = ['name', 'ingredients'];
-  for (let i=0; i<requiredFields.length; i++) {
-    const field = requiredFields[i];
-    if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`
-      console.error(message);
-      return res.status(400).send(message);
-    }
-  }
-  const item = Recipes.create(req.body.name, req.body.ingredients);
-  res.status(201).json(item);
-});
-
-app.put('/recipes/:id', jsonParser, (req, res) => {
-  const requiredFields = ['name', 'ingredients', 'id'];
-  for (let i=0; i<requiredFields.length; i++) {
-    const field = requiredFields[i];
-    if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in Recipe request body`
-      console.error(message);
-      return res.status(400).send(message);
-    }
-  }
-  if (req.params.id !== req.body.id) {
-    const message = (
-      `Request path id (${req.params.id}) and Receipe id `
-      `(${req.body.id}) must match`);
-    console.error(message);
-    return res.status(400).send(message);
-  }
-  console.log(`Updating recipe item \`${req.params.id}\``);
-  const updatedItem = Recipes.update({
-    id: req.params.id,
-    name: req.body.name,
-    ingredients: req.body.ingredients
-  });
-  res.status(204).json(updatedItem);
-});
-
-app.delete('/recipes/:id', (req, res) => {
-  Recipes.delete(req.params.id);
-  console.log(`Deleted recipe \`${req.params.ID}\``);
   res.status(204).end();
 });
 
